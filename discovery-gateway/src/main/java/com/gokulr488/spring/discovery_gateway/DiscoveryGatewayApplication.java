a6a28b6423defa9a87d2f9edc4c510cfffb9f1ed
Gokul Raj <gokulr488@gmail.com>
2024-07-30 16:45:03 +0530

    api routing via api gateway using service ID tested and working

--- discovery-gateway/src/main/java/com/gokulr488/spring/discovery_gateway/DiscoveryGatewayApplication.java
+++ discovery-gateway/src/main/java/com/gokulr488/spring/discovery_gateway/DiscoveryGatewayApplication.java
@@ -2,10 +2,12 @@ package com.gokulr488.spring.discovery_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class DiscoveryGatewayApplication {
 
 	public static void main(String[] args) {
