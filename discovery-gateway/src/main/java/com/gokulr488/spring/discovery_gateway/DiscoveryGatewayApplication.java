da17005fd7b2e82fe0e5d7c61bfe40f9797e07a5
Gokul Raj <gokulr488@gmail.com>
2024-07-30 13:25:22 +0530

    Discovery service configurations

--- discovery-gateway/src/main/java/com/gokulr488/spring/discovery_gateway/DiscoveryGatewayApplication.java
+++ discovery-gateway/src/main/java/com/gokulr488/spring/discovery_gateway/DiscoveryGatewayApplication.java
@@ -2,8 +2,10 @@ package com.gokulr488.spring.discovery_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryGatewayApplication {
 
 	public static void main(String[] args) {
---
+++ payments/src/main/java/com/gokulr488/spring/payments/rest/PaymentsController.java
@@ -0,0 +1,14 @@
+package com.gokulr488.spring.payments.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentsController {
+
+	@GetMapping("/helloWorld")
+	public String helloWorld() {
+		return "Hello world from Service A!";
+	}
+
+}
